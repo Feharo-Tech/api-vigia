70407e496e3ca00cdd9ba0765b96ba98bb51f920
Felipe Haro <feaquino1993@gmail.com>
2025-12-09 15:00:21 -0300

    fix: registro do job no routes/console.php

--- app/Jobs/CheckApiStatusJob.php
+++ app/Jobs/CheckApiStatusJob.php
@@ -2,7 +2,6 @@
 
 namespace App\Jobs;
 
-
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
@@ -10,7 +9,6 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Bus\Queueable;
 use Illuminate\Support\Facades\Artisan;
 
-#[\Illuminate\Console\Scheduling\Schedule('everyMinute')]
 class CheckApiStatusJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Jobs\CheckApiStatusJob;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    dispatch(new CheckApiStatusJob)
+        ->onConnection('redis')
+        ->onQueue('default');
+})->everyMinute();
